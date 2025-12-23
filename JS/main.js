110e2a7c60eb262d5b0a9fad63a52980311dde68
nedasangtarash <neda.sangtarash@gmail.com>
2025-12-23 17:35:15 +0330

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,32 @@
+// ===== PANEL SWITCH =====
 function showPanel(id){
   document.querySelectorAll('.panel').forEach(p=>{
     p.classList.remove('active');
   });
-
-  document.getElementById(id).classList.add('active');
+  const panel = document.getElementById(id);
+  panel.classList.add('active');
 
   const hero = document.getElementById('heroCircle');
-  hero.style.display = (id === 'home') ? 'block' : 'none';
+  hero.style.display = (id==='home') ? 'block' : 'none';
 
-  window.scrollTo({top:0,behavior:'smooth'});
+  window.scrollTo({top:0, behavior:'smooth'});
 }
 
+// ===== PORTFOLIO SWITCH =====
 function showPortfolio(id){
   document.querySelectorAll('.portfolio-section').forEach(s=>{
     s.style.display='none';
   });
   document.getElementById(id).style.display='flex';
+}
+
+// ===== FULLSCREEN PORTFOLIO =====
+function openFullscreen(card){
+  const clone = card.cloneNode(true);
+  clone.classList.add('fullscreen');
+  document.body.appendChild(clone);
+
+  clone.addEventListener('click', ()=>{
+    document.body.removeChild(clone);
+  });
 }
